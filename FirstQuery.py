f006c08451f3952c849a30a20702c862c875581c
eledelaf <eledelaf@ucm.es>
2025-07-27 16:21:22 +0100

    URLS

--- FirstQuery.py
+++ FirstQuery.py
@@ -4,4 +4,4 @@ import pandas as pd
 df = pd.read_csv("/Users/elenadelafuente/Desktop/MASTER/TFM/Project/Project-Master/URLS.csv", sep = ';')  # or pd.read_csv("converted_file.csv")
 print(df.head())
 
-# We are only interested in this papers: 
+# We are only interested in this papers: theguardian.com, 
