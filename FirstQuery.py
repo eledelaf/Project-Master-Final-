341bfd4ca0d46649de4ad1449cdaff195b0bf9b8
eledelaf <eledelaf@ucm.es>
2025-07-27 20:49:35 +0100

    He visto que the times no esta en la base de URLS

--- FirstQuery.py
+++ FirstQuery.py
@@ -1,7 +1,20 @@
 import pandas as pd 
 
 # Upload the data set of URLS
-df = pd.read_csv("/Users/elenadelafuente/Desktop/MASTER/TFM/Project/Project-Master/URLS.csv", sep = ';')  # or pd.read_csv("converted_file.csv")
-print(df.head())
+file_path = "/Users/elenadelafuente/Desktop/MASTER/TFM/Project/Project-Master/URLS.csv"
+df = pd.read_csv(file_path, sep=';', skiprows=1, header=None)
+df = df.iloc[1:].reset_index(drop=True)
+header = [
+    "id", "indexed_date", "language", "media_name", "media_url", 
+    "publish_date", "title", "url"
+]
+df.columns = header
+print(df)
+# We are only interested in this papers: The times, The Guardian, The telegraph
+# Lets see which are the mediar_urls
+conteo = df["media_url"].value_counts()
+repetidos = conteo[conteo > 100]
+print(repetidos)
 
-# We are only interested in this papers: theguardian.com, 
+# No aparece the times(conservador), asi que tengo que buscar otro periodico que sea de la misma mentalidad
+# theguardian.com, telegraph.co.uk, 
