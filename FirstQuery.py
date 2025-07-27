102b5292b31b9cc31f76f394827bf91de2b88d00
eledelaf <eledelaf@ucm.es>
2025-07-27 16:20:29 +0100

    Descargar URLS

---
+++ FirstQuery.py
@@ -0,0 +1,7 @@
+import pandas as pd 
+
+# Upload the data set of URLS
+df = pd.read_csv("/Users/elenadelafuente/Desktop/MASTER/TFM/Project/Project-Master/URLS.csv", sep = ';')  # or pd.read_csv("converted_file.csv")
+print(df.head())
+
+# We are only interested in this papers: 
