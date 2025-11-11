8465a86b3c45b0e9353caf972a1b4cfdc64437c3
eledelaf <eledelaf@ucm.es>
2025-11-11 17:25:42 +0000

    created prueba_scraple para ver si funciona la cosa para limpiar

---
+++ prueba_scraple.py
@@ -0,0 +1,10 @@
+from fun_scraple import scrape_and_text
+from fun_scraple import clean_middle_article
+
+
+URL = 'https://www.standard.co.uk/lifestyle/sauna-etiquette-walhamstow-b1210234.html'
+TITLE = 'A guide to sauna etiquette for Londoners... here\'s how not to embarras yourself'
+TEXT = scrape_and_text(URL, TITLE)
+text = clean_middle_article(TEXT, URL, TITLE)
+print(text)
+
